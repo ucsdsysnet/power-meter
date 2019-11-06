ce427cbf761abdd531aba284fa3030976d8ccee8
malcolmmcswain <mcswain1@pacbell.net>
2019-11-06 13:13:42 -0800

    Refactor into single script

--- read_power.py
+++ read_power.py
@@ -1,43 +1,37 @@
 import vxi11, sched, time, random, string
-
 import numpy as numpy
 import pandas as pd
 
-# Use Agg instead of Xwindows or run ssh -X (comment out these lines)
-import matplotlib
-matplotlib.use('Agg')
+# Using Agg instead of Xwindows or run ssh -X 
+# (comment out these lines if you are running locally)
+# import matplotlib
+# matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
-
 
 # Define instrument
 instr = vxi11.Instrument("172.19.222.92","gpib0,12")
 print(instr.ask("*IDN?"))
 
-# Generate session id and create output file
+# Generate session id and create output file in /data
 session_id = ''.join([random.choice(string.ascii_letters + string.digits) for n in xrange(10)])
 f = open("data/session-"+session_id+".txt", "w")
-
-"""
-# Store session id in a file
-name = open("data/id.txt", "w")
-name.write("%s" % session_id)
-name.close()
-"""
-
 print("Beginning read session: "+session_id)
 
-# Read power over time interval (set to 1s)
+# Define scheduler and collect user input for timeframe
 s = sched.scheduler(time.time, time.sleep)
 secs = float(raw_input("Enter number of seconds: "))
 end = time.time() + secs
 
+# Define read function, query instrument
 def read_power(sc):
-    f.write(instr.ask("measure:scalar:power:real? 0")+",")
+    power = instr.ask("measure:scalar:power:real? 0")
+    f.write(power+",")
     f.flush()
     if time.time() < end:
       s.enter(1, 1, read_power, (sc,))
 
+# Schedule execution over user-inputted timeframe
 s.enter(1, 1, read_power, (s,))
 s.run()
 
